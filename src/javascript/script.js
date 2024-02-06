4e42a2c6531961830001504ef26a505ac131879c
Pedro Henrique Ferreira Graça <pedro.henrrique474@gmail.com>
2024-02-05 22:10:21 -0300

    update

---
+++ src/javascript/script.js
@@ -0,0 +1,5 @@
+document.getElementById('open_btn').addEventListener('click', function() {
+    document.getElementById('sidebar').classList.toggle("open-sidebar");
+
+    /* toggle é igual a IF (se tiver a classe open-sidebar)*/
+})
